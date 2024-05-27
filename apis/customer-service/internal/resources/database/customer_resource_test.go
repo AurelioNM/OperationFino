0e5aa5b9e99c6067c41f0f4626d2c79cf5c855f9
Aurelio_Mororo <aurelio@globalfy.com>
2024-05-27 07:03:38 -0300

    test: unit test on gateway Get functions

--- apis/customer-service/internal/resources/database/customer_resource_test.go
+++ apis/customer-service/internal/resources/database/customer_resource_test.go
@@ -4,36 +4,92 @@ import (
 	"cmd/customer-service/internal/domain/entity"
 	"cmd/customer-service/mocks"
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
-func TestGetCustomerByID(t *testing.T) {
-	// Create a new instance of the mock CustomerGateway
-	mockCustomerGateway := new(mocks.CustomerGateway)
-
-	// Create a sample customer
-	customerID := "customerID"
-	expectedCustomer := &entity.Customer{
+var customerGtw = new(mocks.CustomerGateway)
+var customerID = "customerID"
+var aCustomer = &entity.Customer{
+	ID:      &customerID,
+	Name:    "John",
+	Surname: "Doe",
+	Email:   "john.doe@example.com",
+}
+var aCustomerArray = []*entity.Customer{
+	{
 		ID:      &customerID,
 		Name:    "John",
 		Surname: "Doe",
-		Email:   "john.doe@example.com",
+		Email:   "john.doe@mock.com",
+	},
+	{
+		ID:      &customerID,
+		Name:    "Aurelio",
+		Surname: "Mororo",
+		Email:   "aurelio@mock.com",
+	},
+}
+
+func Test_CustomerGtw_GetCustomerByID(t *testing.T) {
+	type args struct {
+		ctx        context.Context
+		customerID string
 	}
 
-	// Set up the expectation for GetCustomerByID
-	mockCustomerGateway.On("GetCustomerByID", mock.Anything, customerID).Return(expectedCustomer, nil)
+	scenarios := []struct {
+		name        string
+		args        args
+		want        *entity.Customer
+		expectedErr error
+	}{
+		{"success", args{context.Background(), customerID}, aCustomer, nil},
+		{"error", args{context.Background(), "errorID"}, nil, errors.New("error")},
+	}
 
-	// Call the method
-	ctx := context.TODO()
-	customer, err := mockCustomerGateway.GetCustomerByID(ctx, customerID)
+	for _, tt := range scenarios {
+		tt := tt
 
-	// Assert the results
-	assert.NoError(t, err)
-	assert.Equal(t, expectedCustomer, customer)
+		customerGtw.On("GetCustomerByID", tt.args.ctx, tt.args.customerID).Return(tt.want, tt.expectedErr)
 
-	// Ensure that the expectations were met
-	mockCustomerGateway.AssertExpectations(t)
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := customerGtw.GetCustomerByID(tt.args.ctx, tt.args.customerID)
+
+			assert.ErrorIs(t, err, tt.expectedErr)
+			assert.Equal(t, tt.want, got)
+			customerGtw.AssertExpectations(t)
+		})
+	}
+}
+
+func Test_CustomerGtw_GetCustomerList(t *testing.T) {
+	type args struct {
+		ctx context.Context
+	}
+
+	scenarios := []struct {
+		name        string
+		args        args
+		want        []*entity.Customer
+		expectedErr error
+	}{
+		{"success", args{context.Background()}, aCustomerArray, nil},
+		{"error", args{context.TODO()}, nil, errors.New("error")},
+	}
+
+	for _, tt := range scenarios {
+		tt := tt
+
+		customerGtw.On("GetCustomerList", tt.args.ctx).Return(tt.want, tt.expectedErr)
+
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := customerGtw.GetCustomerList(tt.args.ctx)
+
+			assert.ErrorIs(t, err, tt.expectedErr)
+			assert.Equal(t, tt.want, got)
+			customerGtw.AssertExpectations(t)
+		})
+	}
 }
